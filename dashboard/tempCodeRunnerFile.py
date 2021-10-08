eaddb810e030d6e5aebdac0eebf555300d53ea0d
Shenghongzhong <howdavidlearncoding@gmail.com>
2021-10-08 19:21:27 +0100

    Ready for Heroku

--- dashboard/tempCodeRunnerFile.py
+++ dashboard/tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-EAF2F1
+from dash import  dcc
+from dash import  html
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
+from dash.html.Div import Div
+from dash.dependencies import Input, Output
+import dash_table
